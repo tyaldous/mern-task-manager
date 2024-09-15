447d0804cb19058fa0542d758fe96f4d9c48a4ff
Tyler Aldous <tyaldous@gmail.com>
2024-09-15 01:51:50 -0600

    added api calls and middleware for API

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,51 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/connectDB");
+const Task = require("./model/taskModel");
 
 const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//const logger = (req, res, next) => {
+//    console.log("Middleware running")
+//    console.log(req.method)
+//    next();
+
+
 
 //Routes
 app.get("/", (req, res) => {
     res.send("Home Page");
 } );
 
-connectDB();
+//Create Task
+app.post("/api/tasks", async(req, res) => {
+    //console.log(req.body);
+    //res.send("Task Created");
+    try {
+        const task = await Task.create(req.body)
+        res.status(200).json(task)
+
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+        //console.log(error);
+    }
+})
 
 const PORT = process.env.PORT || 5000;
-app.listen("5000", () => {
-    console.log(`Server running on port ${PORT}`)
-} );
 
-//mongodb+srv://tyaldous:mern_test@mern.7bmxa.mongodb.net/?retryWrites=true&w=majority&appName=mern
+const startServer = async () => {
+    try{
+        await connectDB();
+        app.listen("5000", () => {
+            console.log(`Server running on port ${PORT}`)
+        } );
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+startServer();
