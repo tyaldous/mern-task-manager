2430a1f2f81bd868d7f5101d49486c1c7605a415
Tyler Aldous <tyaldous@gmail.com>
2024-09-14 23:24:06 -0600

    Adding route to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+//Routes
+app.get("/", (req, res) => {
+    res.send("Home Page");
+} );
+
 const PORT = process.env.PORT || 5000;
 app.listen("5000", () => {
     console.log(`Server running on port ${PORT}`)
