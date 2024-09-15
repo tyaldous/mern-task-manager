26ba91e1887a5c51e7e7648428bf2e7c6eab4c6d
Tyler Aldous <tyaldous@gmail.com>
2024-09-15 00:33:01 -0600

    adding mongo connection

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/connectDB");
 
 const app = express();
 
@@ -6,6 +8,8 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Home Page");
 } );
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen("5000", () => {
