902d95d1b6be3d01fe999c92699541d98eec198e
Tyler Aldous <tyaldous@gmail.com>
2024-09-15 00:16:35 -0600

    adding mongo

--- backend/server.js
+++ backend/server.js
@@ -11,3 +11,5 @@ const PORT = process.env.PORT || 5000;
 app.listen("5000", () => {
     console.log(`Server running on port ${PORT}`)
 } );
+
+//mongodb+srv://tyaldous:mern_test@mern.7bmxa.mongodb.net/?retryWrites=true&w=majority&appName=mern
